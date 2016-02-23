68e65f1f45492560fc308b60d595bca897fd564d
ginteractivenyc <gregatria@gmail.com>
2016-02-23 17:28:18 -0500

    latest

--- js/controllers.js
+++ js/controllers.js
@@ -7,7 +7,15 @@ gregApp.controller('homeCtrl', function($scope, $rootScope){
 gregApp.controller('workCtrl', function($scope){
 	console.log("WORK")
 	$scope.work = [
+		{
+			product: "DeepDive Microtool",
+			description:"Lorem ipsum dolor sit amet, mauris ac lacus vel pellentesque nulla augue, nec massa eget, pede ad odio liber",
+			thumb:"thumb3.jpg",
+			url:"http://abcnews.go.com/Politics/fullpage/running-mates-2016-presidential-candidates-spouses-36167995",
+			tech:"AngularJS, JSON",
+			client:"Disney / ABC News"
 
+		},	
 		{
 			product: "Oscars Brackets",
 			description:"Lorem ipsum dolor sit amet, mauris ac lacus vel pellentesque nulla augue, nec massa eget, pede ad odio liber",
@@ -27,18 +35,9 @@ gregApp.controller('workCtrl', function($scope){
 
 		},		
 		{
-			product: "DeepDive Microtool",
-			description:"Lorem ipsum dolor sit amet, mauris ac lacus vel pellentesque nulla augue, nec massa eget, pede ad odio liber",
-			thumb:"thumb3.jpg",
-			url:"http://abcnews.go.com/Politics/fullpage/running-mates-2016-presidential-candidates-spouses-36167995",
-			tech:"AngularJS, JSON",
-			client:"Disney / ABC News"
-
-		},
-		{
 			product: "Notesday",
 			description:"Lorem ipsum dolor sit amet, mauris ac lacus vel pellentesque nulla augue, nec massa eget, pede ad odio liber",
-			thumb:"thumb4.jpg",
+			thumb:"thumb6.jpg",
 			url:"http://abcnews.go.com/Politics/fullpage/running-mates-2016-presidential-candidates-spouses-36167995",
 			tech:"AngularJS, JSON",
 			client:"Disney / ABC News"
